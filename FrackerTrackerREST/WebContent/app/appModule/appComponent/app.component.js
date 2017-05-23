39ca1ab8c13ccc70d904694a15b335f29e8a023c
StefanFuller147 <sfullersywn@gmail.com>
2017-05-23 16:15:53 -0600

    have an injector error somewhere...IDK where...fml

---
+++ FrackerTrackerREST/WebContent/app/appModule/appComponent/app.component.js
@@ -0,0 +1,8 @@
+angular.module("appModule").component("appComponent", {
+    templateUrl : 'app/appModule/appComponent/app.component.html',
+    controller: function(authService, $location) {
+    	var vm = this;
+    	
+    },
+    controllerAs: 'vm'
+    });
